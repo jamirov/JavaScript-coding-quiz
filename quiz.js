5242fad45faca44c3008ee0893a79d669efca3fb
Jasur Amirov <ajasur@gmail.com>
2021-01-21 23:01:07 -0800

    JS add final score fixed

--- quiz.js
+++ quiz.js
@@ -19,7 +19,9 @@ var wrongAlert = document.getElementById("resultAlertWrong");
 var score = 0;
 var penalty = 10;
 var timeleft = 75;
-
+var finalScore = document.getElementById("score");
+var initial = document.getElementById("initial");
+var displayScore = document.getElementById("displayScore");
 
 button.addEventListener("click", function() {
 var downloadTimer = setInterval(function(){
@@ -37,13 +39,10 @@ h1.setAttribute("style", "display:none");
 question1.setAttribute("style","display:inline-block");
 });
 
-
-
 questionOne.addEventListener("click", function(event) {
 var element = event.target;
 if (element == answer1){
     score++;
-    console.log(score);
     resultAlert.setAttribute("style","display:inline-block");
     wrongAlert.setAttribute("style","display:none");       
 } else {
@@ -54,7 +53,6 @@ if (element == answer1){
 questionOne.setAttribute("style","display:none");
 questionTwo.setAttribute("style","display:inline-block");
 });
-
 
 questionTwo.addEventListener("click", function(event) {
     var element = event.target;
@@ -88,15 +86,12 @@ questionThree.setAttribute("style","display:none");
 questionFour.setAttribute("style","display:inline-block");
 });
 
-
-
 questionFour.addEventListener("click", function(event) {
     var element = event.target;
 if (element == answer4){
     score++;
-    console.log(score);
     resultAlert.setAttribute("style","display:inline-block");
-    wrongAlert.setAttribute("style","display:none");       
+    wrongAlert.setAttribute("style","display:none");
 } else {
     wrongAlert.setAttribute("style","display:inline-block");
     resultAlert.setAttribute("style","display:none")
@@ -104,18 +99,23 @@ if (element == answer4){
 }
 questionFour.setAttribute("style","display:none");
 allDone.setAttribute("style","display:inline-block");
+score = timeleft;
+finalScore.innerHTML = score;
+timeleft = 0;
 });
-
-
 
 document.getElementById("submit").addEventListener("click", function() {
-allDone.setAttribute("style","display:none");
 resultAlert.setAttribute("style","display:none");
-highScoresBoard.setAttribute("style","display:inline-block");
+var newScore = {init:initial.value, score:score}
+localStorage.setItem("newScore", JSON.stringify(newScore));
+var stringObj = JSON.parse(localStorage.getItem('newScore'));
+var int = stringObj.init;
+var scor = stringObj.score;
+var newLi = document.createElement("li");
+newLi.innerHTML = int + " " + scor;
+var scoreBoard = document.getElementById("scoreBoard");
+scoreBoard.appendChild(newLi);
 });
-
-
-
 
 viewHighScores.addEventListener("click", function() {
 allDone.setAttribute("style","display:none");
@@ -128,10 +128,9 @@ pEl.setAttribute("style", "display:none");
 h1.setAttribute("style", "display:none");
 resultAlert.setAttribute("style","display:none");
 highScoresBoard.setAttribute("style","display:inline-block");
+var initial2 = (JSON.parse(localStorage.getItem('newScore')));
+console.log(initial2);
 });     
-
-
-
 
 document.getElementById("goBack").addEventListener("click", function() {
 location.reload();
